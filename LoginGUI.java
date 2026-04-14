7d4486fb1816a7ee9f85a93fd1c7d5c5a23e1852
FilipWojciuch <2505614@live.stmarys.ac.uk>
2026-04-14 14:32:26 +0100

    first commit

---
+++ LoginGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginGUI{
+   public LoginGUI(){
+    JFrame frame = new JFrame("LoginGUI"); 
+    frame.setSize(400, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLayout(new FlowLayout());
+    frame.setVisible(true);
+}
+}
