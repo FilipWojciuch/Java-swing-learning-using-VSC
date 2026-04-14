ed43928bc670fbdec06b01418a4418d9ba1bf6eb
FilipWojciuch <2505614@live.stmarys.ac.uk>
2026-04-14 14:54:28 +0100

    Added a button, label, checkbox and radiobutton example as a start in making a GUI.

--- LoginGUI.java
+++ LoginGUI.java
@@ -3,10 +3,18 @@ import java.awt.*;
 
 public class LoginGUI{
    public LoginGUI(){
-    JFrame frame = new JFrame("LoginGUI"); 
-    frame.setSize(400, 300);
+    JFrame frame = new JFrame("LoginGUI");
+    JButton button = new JButton("Click Me");
+    JLabel label = new JLabel("Hello, Swing!");
+    JCheckBox checkbox = new JCheckBox("Check Me");
+    JRadioButton radiobutton = new JRadioButton("Select me");
+    frame.setSize(150, 300);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new FlowLayout());
+    frame.add(button);
+    frame.add(label);
+    frame.add(checkbox);
+    frame.add(radiobutton);
     frame.setVisible(true);
 }
 }
